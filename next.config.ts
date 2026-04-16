1d4a675de3e1555ad296763155393670a0f1aae1
Diego-programand <juandiegofigueroacardenas069@gmail.com>
2026-04-15 19:56:07 -0500

    config(next): add cdn.sanity.io to image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         hostname: 'res.cloudinary.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '/**',
+      },
     ],
     formats: ['image/avif', 'image/webp'], // Formatos modernos
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
